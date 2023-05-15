3868671c565ad71a9afaa7f05216affcab4a092e
Morgon-GH <moritz.olthoff@gmail.com>
2023-05-15 21:57:18 +0200

    holder moved

---
+++ src/main/java/de/morgon/missile/inventories/holder/MenuHolder1.java
@@ -0,0 +1,14 @@
+package de.morgon.missile.inventories.holder;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.jetbrains.annotations.NotNull;
+
+public class MenuHolder1 implements InventoryHolder {
+    @Override
+    public @NotNull Inventory getInventory() {
+        return null;
+    }
+}
+
+
